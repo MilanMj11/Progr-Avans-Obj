fe4fd8a8be325461af1aac0e16abcee7a3f790f6
Mujdar Milan <mujdar.milan@gmail.com>
2024-04-23 21:48:40 +0300

    lab8

---
+++ lab8/lab8PAOmaven/src/main/java/Main.java
@@ -0,0 +1,28 @@
+import smartCalculator.CalculatorRequest;
+import smartCalculator.CalculatorResult;
+import smartCalculator.SmarterCalculator;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String url = "jdbc:mysql://localhost:3306/laborator";
+        String username = "student";
+        String password = "student";
+
+        SmarterCalculatorSaver resultSaver = new SmarterCalculatorSaver(url, username, password);
+
+        args = new String[]{"1", "+", "2", "2", "*", "5", "1", "+", "0.5", "1.0", "-", "2.0", "10.0", "/", "1", "true", "&&", "false"};
+
+        List<CalculatorResult> results = SmarterCalculator.calculate(args);
+
+        for (CalculatorResult res : results) {
+            CalculatorRequest req = res.getRequest();
+            Object rasp = res.computeResult();
+
+            System.out.println("Operation " + req + " has result " + rasp);
+            resultSaver.saveResult(req, res);
+        }
+    }
+}
---
+++ lab8/lab8PAOmaven/src/main/java/smartCalculator/CalculatorResult.java
@@ -0,0 +1,22 @@
+package smartCalculator;
+
+public class CalculatorResult {
+    private final CalculatorRequest calculatorRequest;
+
+    protected CalculatorResult(CalculatorRequest calculatorRequest){
+        this.calculatorRequest = calculatorRequest;
+    }
+
+    public CalculatorRequest getRequest(){
+        return calculatorRequest;
+    }
+
+    public Object computeResult(){
+        return null;
+    }
+
+    @Override
+    public String toString(){
+        return getRequest().toString() + " = " + computeResult();
+    }
+}
